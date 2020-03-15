0fc314ec66d5bf04f3443a41e4b328a7c087825f
Brandon Conway <brandoncc@gmail.com>
2020-03-15 09:15:40 -0700

    Change character strategy when changing modes

--- src/objects/mode.ts
+++ src/objects/mode.ts
@@ -3,6 +3,17 @@ import PositionComponent from './components/position';
 import GlobalContext, {LocalContext, ScreenType} from '../context';
 import {EventType} from '../events';
 import StyledCharacterStrategy from '../characters/styled';
+import BasicCharacterStrategy from '../characters/basic';
+
+const characterStrategies = {
+    [ScreenType.Normal]: new StyledCharacterStrategy({
+        bold: true,
+        fg: '96A537',
+        bg: 'FF0000'
+    }),
+    [ScreenType.Insert]: new BasicCharacterStrategy(),
+    [ScreenType.MainMenu]: new BasicCharacterStrategy()
+};
 
 export default class Mode {
     public entity: EntityItem;
@@ -19,16 +30,13 @@ export default class Mode {
         );
 
         this.position.setCharacterStrategy(
-            new StyledCharacterStrategy({
-              bold: true,
-              fg: '96A537',
-              bg: 'FF0000'
-            }),
-        );
+          characterStrategies[ScreenType.MainMenu]);
 
         context.store.attachComponent(this.entity, this.position);
         context.events.on(evt => {
             if (evt.type === EventType.ScreenTypeChanged) {
+                this.position.setCharacterStrategy(
+                  characterStrategies[context.screen]);
                 this.position.setChar(context.screen);
             }
         });
