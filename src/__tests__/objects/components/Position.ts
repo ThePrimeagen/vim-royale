b7ad442e038e6edaae2f0d041b75d840a34538b6
Brandon Conway <brandoncc@gmail.com>
2020-03-15 04:19:53 -0700

    Set correct test scope description

--- src/__tests__/objects/components/Position.ts
+++ src/__tests__/objects/components/Position.ts
@@ -2,12 +2,10 @@ import DefaultCharacterStrategy from "../../../characters/default";
 import PositionComponent from "../../../objects/components/position";
 
 describe('PositionComponent', () => {
-    describe('setChar', () => {
-        test('uses default character strategy by default', () => {
-            const position = new PositionComponent('', 0, 0);
+    test('uses default character strategy by default', () => {
+        const position = new PositionComponent('', 0, 0);
 
-            expect(position.characterStrategy)
-              .toBeInstanceOf(DefaultCharacterStrategy);
-        })
-    });
+        expect(position.characterStrategy)
+            .toBeInstanceOf(DefaultCharacterStrategy);
+    })
 });
